3798881af69e73b2e3b04a3e8fea953f53cd12a1
nntesla <nntesla148@gmail.com>
2021-10-19 14:16:12 +0300

    Profile

--- bookmarks4/settings.py
+++ bookmarks4/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashborad'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
